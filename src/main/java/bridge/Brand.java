eeaa6598cce35459b0a52879a562381bab201acd
Jyf010205 <jianyufeng@agree.com.cn>
2020-05-20 15:34:46 +0800

    重新构成maven项目

---
+++ src/main/java/bridge/Brand.java
@@ -0,0 +1,10 @@
+package bridge;
+
+/**
+ * @author: jianyufeng
+ * @description: 接口
+ * @date: 2020/5/20 15:26
+ */
+public interface Brand {
+
+}
---
+++ src/main/java/bridge/Phone.java
@@ -0,0 +1,14 @@
+package bridge;
+
+/**
+ * @author: jianyufeng
+ * @description: 实现类
+ * @date: 2020/5/20 15:27
+ */
+public abstract class Phone {
+    Brand brand;
+
+    public Phone(Brand brand){
+        this.brand = brand;
+    }
+}
